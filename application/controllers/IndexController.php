128abc2fda6b3a799c2103ec855cd79370ab51d2
Alberto Alcolea <albertoalcolea@gmail.com>
2012-07-13 19:54:29 +0200

    added first admin function

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,6 +6,8 @@ class IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+        $this->view->baseUrl = $this->_request->getBaseUrl();
+        $this->view->user = Zend_Auth::getInstance()->getIdentity();
     }
 
     public function indexAction()
@@ -14,5 +16,28 @@ class IndexController extends Zend_Controller_Action
 		
 		$this->view->editorials = $table->fetchAll();	
     }
+	
+	/* Admin panel */
+    public function adminAction()
+    {
+		$auth = Zend_Auth::getInstance();
+		if (!$auth->hasIdentity()) {
+			$this->_redirect('/login');
+		} 
+		
+		$this->_helper->layout()->setLayout('admin');
+		
+		$userInfo = $auth->getStorage()->read();
+		$this->view->username = $userInfo->username;
+		
+		/*$users = new Application_Model_DbTable_Users();
+		$datos = $users->getUsuarios();
+		$this->view->usuarios = $datos;
+       
+		$posts = new Application_Model_DbTable_Post();
+		$post = $posts->listar();
+		$this->view->datos = $post;*/
+    }
+	
 }
 
